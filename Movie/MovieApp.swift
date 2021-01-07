f4d7cc331c5f13efeb8d3509dd8c849df2549656
FillipiPS <fillipi_ps@outlook.com>
2021-01-06 22:21:58 -0300

    Initial commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Fillipi Paiva Suszek on 06/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
